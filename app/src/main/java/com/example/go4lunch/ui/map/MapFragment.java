25a223e9d876f292bd08fb26fa805e484e1c8a06
Guillaume Cudel <guillaumecudel@chromecast.home>
2021-05-15 11:46:15 +0200

    First commit

---
+++ app/src/main/java/com/example/go4lunch/ui/map/MapFragment.java
@@ -0,0 +1,25 @@
+package com.example.go4lunch.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.go4lunch.R;
+
+public class MapFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/go4lunch/ui/workmates/WorkmatesFragment.java
@@ -0,0 +1,31 @@
+package com.example.go4lunch.ui.workmates;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.go4lunch.R;
+
+public class WorkmatesFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_workmates, container, false);
+        return view;
+
+    }
+}
